8a5b88eb7a730930246c429cc6be3211f9f3db18
siddharth <sid9057819561@gmail.com>
2021-12-10 00:02:28 +0530

    sid

--- hello/settings.py
+++ hello/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sid1'
+    'sid1',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- sid1/views.py
+++ sid1/views.py
@@ -1,5 +1,18 @@
+
 from django.shortcuts import render,HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
-def method1(requets):
-    return HttpResponse("hii sid")
+@api_view(['POST'])
+def method1(request):
+    code=request.data.get('code')
+    len1=request.data.get('len')
+    context={
+        'msg':'we got data',
+         'code':code,
+         'len':len1
+         
+    }
+
+    return Response(context)
